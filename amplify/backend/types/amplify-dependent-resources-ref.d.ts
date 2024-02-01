22a73449b2a5edb2bea19080b61c451e9fd4dda8
Danny Forest <ford86@gmail.com>
2024-02-01 09:11:05 -0500

    Add production configuration to Amplify backend
    This commit adds configuration data for a production environment in 'team-provider-info.json' and updates the 'AmplifyDependentResourcesAttributes' type in 'amplify-dependent-resources-ref.d.ts' in Amplify backend. The production config includes information related to cloudformation, authentication roles, region, deployment bucket and more. These changes are necessary to ensure the application works correctly in a production environment.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,17 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "questions": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "questions": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
