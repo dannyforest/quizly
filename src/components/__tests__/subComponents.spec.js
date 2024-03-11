8238752827391fb7ed15948d5bd1d23347b29b06
Carl Drolet <kin_saddi97@hotmail.com>
2024-03-11 18:09:48 -0400

    reformated

--- src/components/__tests__/subComponents.spec.js
+++ src/components/__tests__/subComponents.spec.js
@@ -9,67 +9,55 @@ import Quiz from '../Quiz.vue'
 import Results from '../Results.vue'
 
 describe.only('QuizView', () => {
-    let wrapper;
-    
-    beforeEach(()=>{
-       
+  let wrapper
+
+  beforeEach(() => {})
+  const categories = 'Select a category'
+  it('verifies that startOptionsComponent exists', () => {
+    wrapper = mount(QuizView, {
+      props: {
+        categories: categories
+      }
     })
-    const categories = 'Select a category'
-    it('verifies that startOptionsComponent exists', () =>{
-        wrapper = mount(QuizView, {props:{
-            categories : categories,
-        }},
-        )  
-        const startOptionsComponent = wrapper.findComponent(StartOptions)
-        expect(startOptionsComponent.text()).toContain(categories)
-        expect(startOptionsComponent.exists()).toBeTruthy() // #7
+    const startOptionsComponent = wrapper.findComponent(StartOptions)
+    expect(startOptionsComponent.text()).toContain(categories)
+    expect(startOptionsComponent.exists()).toBeTruthy() // #7
+  })
+  it('verifies that Leaderboards exists', () => {
+    wrapper = mount(QuizView, {
+      props: {
+        categories: categories
+      },
+      components: {
+        Leaderboards
+      }
     })
-    it('verifies that Leaderboards exists', ()=>{
-        wrapper = mount(QuizView, {props:{
-            categories : categories,
-        },
-        components:{
-            Leaderboards,
-        }},
-        )  
-        const LeaderboardsComponent = wrapper.findComponent(Leaderboards)
-        expect(LeaderboardsComponent.exists()).toBeTruthy() // #8
+    const LeaderboardsComponent = wrapper.findComponent(Leaderboards)
+    expect(LeaderboardsComponent.exists()).toBeTruthy() // #8
+  })
+  
+  describe('testStarted', async () => {
+    it('verifies that Timer exists', async () => {
+      wrapper.vm.quizStarted = true
+      await wrapper.vm.$nextTick()
+      const TimerComponent = wrapper.findComponent(Timer)
+      expect(TimerComponent.exists()).toBeTruthy() // #9
     })
-    describe('testStarted', async () =>{
-        it('verifies that Timer exists', async ()=>{
-            wrapper = mount(QuizView, {
-                props: {
-                    selectedTimeLimit: 60
-                }
-            })
-            wrapper.vm.quizStarted = true;
-            await wrapper.vm.$nextTick()
-            const TimerComponent = wrapper.findComponent(Timer)
-            expect(TimerComponent.exists()).toBeTruthy() // #9
-        })
-        it('verifies thatProgressBar exists', ()=>{
-            
-            wrapper.vm.currentQuestionIndex = 0;
-            const ProgressBarComponent = wrapper.findComponent(ProgressBar)
-            expect(ProgressBarComponent.exists()).toBeTruthy() // #10
-        })
-        it('verifies that Quiz exists', ()=>{
-            
-            const QuizComponent = wrapper.findComponent(Quiz)
-            expect(QuizComponent.exists()).toBeTruthy() // #11
-        })
-        it('verifies that Results exists', async ()=>{
-            wrapper = mount(QuizView,
-                {
-                    props: {
-                        categories: 'Literature',
-                    }
-                })
-                wrapper.vm.quizStarted = true;
-                wrapper.vm.showResults = true;
-                await wrapper.vm.$nextTick()
-            const ResultsComponent = wrapper.findComponent(Results)
-            expect(ResultsComponent.exists()).toBeTruthy() // #12
-        })
+    it('verifies thatProgressBar exists', () => {
+      wrapper.vm.currentQuestionIndex = 0
+      const ProgressBarComponent = wrapper.findComponent(ProgressBar)
+      expect(ProgressBarComponent.exists()).toBeTruthy() // #10
     })
+    it('verifies that Quiz exists', () => {
+      const QuizComponent = wrapper.findComponent(Quiz)
+      expect(QuizComponent.exists()).toBeTruthy() // #11
+    })
+    it('verifies that Results exists', async () => {
+      wrapper.vm.quizStarted = true
+      wrapper.vm.showResults = true
+      await wrapper.vm.$nextTick()
+      const ResultsComponent = wrapper.findComponent(Results)
+      expect(ResultsComponent.exists()).toBeTruthy() // #12
+    })
+  })
 })
